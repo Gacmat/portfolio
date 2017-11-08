ce5b69df76c7b501310d8f91be7e5c841e518454
Gacu007 <33404506+Gacu007@users.noreply.github.com>
2017-11-09 00:41:21 +0100

    Update Game.java
    Jest już bufor i czarny ekran.

--- Game.java
+++ Game.java
@@ -1,7 +1,11 @@
 package SeriousGame;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
 import javax.swing.JFrame;
 
 public class Game extends Canvas implements Runnable{
@@ -15,18 +19,19 @@ public class Game extends Canvas implements Runnable{
 	private JFrame frame;
 	private boolean running = false;
 	
+	// -------GAME----------------------------------------------GAME
 	public Game(){	
 		Dimension size = new Dimension(width*scale, height*scale);
 		setPreferredSize(size);
 		frame = new JFrame();
 	}
-	
+	// -------start----------------------------------------------start
 	public synchronized void start(){
 		running = true;
 		thread = new Thread(this, "Display");
 		thread.start();
 	}
-	
+	// -------stop------------------------------------------------stop
 	public synchronized void stop(){
 		running = false;
 		try{
@@ -36,14 +41,33 @@ public class Game extends Canvas implements Runnable{
 			e.printStackTrace();
 		}
 	}
-	
+	// -------run--------------------------------------------------run
 	public void run(){
 		new Game();
 		while(running){
-			System.out.println("Running...");
-		
+			update();
+			render();
 		}
 	}
+	// -------update----------------------------------------------update
+	public void update(){	
+	}
+	// -------render----------------------------------------------render
+	public void render(){
+		BufferStrategy bs = getBufferStrategy();
+		if (bs == null){
+			createBufferStrategy(3);
+			return;
+		}
+		Graphics g = bs.getDrawGraphics();
+		// Grafika start
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		// Grafika stop
+		g.dispose();
+		bs.show();
+	}
+	// -------main------------------------------------------------main
 	public static void main(String[] args) {
 		Game game = new Game();
 		game.frame.setResizable(false);
@@ -56,15 +80,4 @@ public class Game extends Canvas implements Runnable{
 		
 		game.start();
 	}
-	
-	public void UPDATE(){	
-	}
-	
-	public void RENDER(){
-	}
-		
-
-	
-
-
 }
